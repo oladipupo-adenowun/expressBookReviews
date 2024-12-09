298f9b927e5f9f67fc9cff9ebedeff4dc0d91178
oladipupo-adenowun <102272731+oladipupo-adenowun@users.noreply.github.com>
2024-12-09 18:26:03 -0500

    implemented the features

--- final_project/index.js
+++ final_project/index.js
@@ -8,10 +8,31 @@ const app = express();
 
 app.use(express.json());
 
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+  // Check if user is authenticated
+  if (req.session.authorization) {
+    let token = req.session.authorization['accessToken']; // Access Token
+    
+    // Verify JWT token for user authentication
+    jwt.verify(token, "access", (err, user) => {
+        if (!err) {
+            req.user = user; // Set authenticated user data on the request object
+            next(); // Proceed to the next middleware
+        } else {
+            return res.status(403).json({ message: "User not authenticated" }); // Return error if token verification fails
+        }
+    });
+    
+    // Return error if no access token is found in the session
+} else {
+    return res.status(403).json({ message: "User not logged in" });
+}
 });
  
 const PORT =5000;
